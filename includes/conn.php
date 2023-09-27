78ae7d0d7e0de552bb306f0e4a81207c84b4af93
evanlreblora <evan.reblora@gmail.com>
2023-09-27 12:58:13 +0800

    5th commit

--- includes/conn.php
+++ includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'libsystem');
+	$conn = new mysqli('localhost', 'root', '', 'kmproject_db');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
