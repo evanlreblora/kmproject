e7de4b1b5e0f9eb5122b292772158081de9d8ec5
evanlreblora <evan.reblora@gmail.com>
2023-10-03 08:43:58 +0800

    8th commit

--- admin/download.php
+++ admin/download.php
@@ -21,37 +21,7 @@
     </section>
     <!-- Main content -->
     <section class="content">
-      <?php
-        if(isset($_SESSION['error'])){
-          ?>
-            <div class="alert alert-danger alert-dismissible">
-              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-              <h4><i class="icon fa fa-warning"></i> Error!</h4>
-                <ul>
-                <?php
-                  foreach($_SESSION['error'] as $error){
-                    echo "
-                      <li>".$error."</li>
-                    ";
-                  }
-                ?>
-                </ul>
-            </div>
-          <?php
-          unset($_SESSION['error']);
-        }
 
-        if(isset($_SESSION['success'])){
-          echo "
-            <div class='alert alert-success alert-dismissible'>
-              <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-              <h4><i class='icon fa fa-check'></i> Success!</h4>
-              ".$_SESSION['success']."
-            </div>
-          ";
-          unset($_SESSION['success']);
-        }
-      ?>
       <div class="row">
         <div class="col-xs-12">
           <div class="box">
--- download.php
+++ download.php
@@ -1,42 +1,48 @@
-<?php include 'includes/session.php'; ?>
+<?php include 'includes/session.php'; 
+
+
+session_start();
+$r=session_id();
+
+/* SOME PIECE OF CODE TO AUTHENTICATE THE USER, MOSTLY SQL QUERY... */
+
+/* now registering a session for an authenticated user */
+$username = $_SESSION['username'];
+// $student_id = $_SESSION['student_id'];
+
+/* now displaying the session id..... */
+echo "the session id id: ".$r;
+echo " and the session has been registered for: ".$_SESSION['username'];
+ ?>
 <?php
+ session_start(); 
+ print_r ($_SESSION);
+ echo "<p>";
+
+ //echo a single entry from the array
+ echo $_SESSION['username'][2];
  
 	if(ISSET($_REQUEST['bookid'])){
 		$bookid = $_REQUEST['bookid'];
-		
+ 
+	
+
 		$query = mysqli_query($conn, "SELECT * FROM `books` WHERE `id` = '$bookid'") or die(mysqli_error());
 		$fetch  = mysqli_fetch_array($query);
 		$filename = $fetch['file'];
-
-
-
-
-				if($fetch->num_rows > 0){
-					$brow = $query->fetch_assoc();
-					$bid = $brow['id'];
-
-					$sql = "INSERT INTO borrow (student_id, book_id, date_borrow) VALUES ('$student_id', '$bid', NOW())";
-					if($conn->query($sql)){
-						$added++;
-						$sql = "UPDATE books SET status = 1 WHERE id = '$bid'";
-						$conn->query($sql);
-					}
-					else{
-						if(!isset($_SESSION['error'])){
-							$_SESSION['error'] = array();
-						}
-						$_SESSION['error'][] = $conn->error;
-					}
-
-				}
-				else{
-					if(!isset($_SESSION['error'])){
-						$_SESSION['error'] = array();
-					}
-					$_SESSION['error'][] = 'Report with ISBN - '.$isbn.' unavailable';
-				}
-
-
+		// $student_id = 2;
+		$status = 1;
+		 
+		$sql = "INSERT INTO borrow (student_id, book_id, date_borrow, status) VALUES ('$student_id', '$bookid', NOW(), '$status')";
+		   if($conn->query($sql)){
+			   $_SESSION['success'] = 'Report added successfully';
+		   }
+		   else{
+			   $_SESSION['error'] = $conn->error;
+		   }
+		   
+   
+ 
 	 
 		header("Content-Disposition: attachment; filename=".$filename);
 		header("Content-Type: application/octet-stream;");
